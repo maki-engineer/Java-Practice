14b793e61825ec43995d969f5303ee0cf9e34189
maki-engineer <ryohei-81@outlook.jp>
2022-09-25 11:54:00 +0900

    add: 継承クラスSuperHeroクラスを定義

---
+++ SuperHero.java
@@ -0,0 +1,11 @@
+public class SuperHero extends Hero {
+  boolean flying;
+
+  public void fly(){
+    System.out.println("飛び上がった！");
+  }
+
+  public void land(){
+    System.out.println("着地した！");
+  }
+}
